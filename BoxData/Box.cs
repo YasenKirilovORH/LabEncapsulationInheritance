7136a41d2227264bd0fe8620962ab0648ecabe1e
YasenKirilovORH <Nightwalkerorh@gmail.com>
2024-08-08 16:07:17 +0300

    Add project files.

---
+++ BoxData/Box.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Text;
+
+namespace BoxData;
+
+public class Box
+{
+    private double length;
+    private double width;
+    private double height;
+
+    public Box (double length, double width, double height)
+    {
+        this.Length = length;
+        this.Width = width;
+        this.Height = height;
+
+    }
+
+    public double Length
+    {
+        get => length;
+        private set
+        {
+            if(value <= 0)
+            {
+                throw new ArgumentException("Length cannot be zero or negative.");
+            }
+            this.length = value;
+        }
+    }
+
+    public double Width
+    {
+        get => width;
+        private set
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException("Width cannot be zero or negative.");
+            }
+            this.width = value;
+        }
+    }
+
+    public double Height
+    {
+        get => height;
+        private set
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException("Height cannot be zero or negative.");
+            }
+            this.height = value;
+        }
+    }
+
+    public double SurfaceArea()
+    {
+        double area = 2 * Length * Width + 2 * Length * Height + 2 * Width * Height;
+
+        return area;
+    }
+
+    public double Volume()
+    {
+        double volume = Length * Width * Height;
+        return volume;
+    }
+
+    public override string ToString()
+    {
+        string output = $"Surface Area – {SurfaceArea():F2}{Environment.NewLine}" +
+                        $"Volume - {Volume():F2}";
+
+        return output;
+    }
+}
---
+++ PersonInfo/Program.cs
@@ -0,0 +1,25 @@
+﻿using PersonInfo;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+int lines = int.Parse(Console.ReadLine()!);
+
+List<Person> persons = new List<Person>();
+for (int i = 0; i < lines; i++)
+{
+    string[] cmd = Console.ReadLine()!.Split();
+    string firstName = cmd[0];
+    string lastName = cmd[1];
+    int age = int.Parse(cmd[2]);
+
+    Person person = new Person(firstName, lastName, age);
+    persons.Add(person);
+}
+
+persons
+    .OrderBy(p => p.FirstName)
+    .ThenBy(p => p.Age)
+    .ToList()
+    .ForEach(Console.WriteLine);
